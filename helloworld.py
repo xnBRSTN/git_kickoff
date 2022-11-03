e35a135ee555983085d07d3acc22478a8548bb4a
xnBRSTN <xnazar@brainstation.io>
2022-11-03 11:24:56 -0400

    updated with wrong comments

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
 ### Kickoff for Nov. 3, 2022
 
 if __name__ == '__main__':
-    x = input("What is your name?")
+    x = input("What is your name?") #wrong comments
     print(f'Hey {x}, nice to meet you!')
